e1ef08ce91d23081ae033ddd671fd5b5e69309fc
MarceC21 <167596913+MarceC21@users.noreply.github.com>
2024-04-20 21:11:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
 import user_management
 import tutoring_management
+opcion="algo"
+while opcion!="3": 
+    print("1. Registrarse")
+    print("2. Iniciar sesión")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+    
+    if opcion == "1":
+        user_management.registrarse()
+    elif opcion == "2":
+        usuario = user_management.iniciar_sesion()
+        if usuario:
+            mensaje_bienvenida = "¡Bienvenido, {}!".format(usuario["correo"])
+            print(mensaje_bienvenida)
+            menu="algo"
+            while menu!="4":
+                print("\nMenú de Tutorías:")
+                print("1. Ver clases disponibles para tutorías")
+                print("2. Ver tutorías asignadas")
+                print("3. Dejar un comentario")
+                print("4. Cerrar sesión")
+                
+                menu = input("Seleccione una opción: ")
+                
+                if menu == "1":
+                    tutoring_management.ver_clases_disponibles()
+                elif menu == "2":
+                    tutoring_management.ver_tutorias_asignadas(usuario)
+                elif menu == "3":
+                    tutoring_management.dejar_comentario(usuario)
+                elif menu == "4":
+                    print("Cerrando sesión...")
+                    break
+                else:
+                    print("Opción inválida. Por favor, inténtelo de nuevo.")
 
-def main():
-    while True:
-        print("1. Registrarse")
-        print("2. Iniciar sesión")
-        print("3. Salir")
-        
-        opcion = input("Seleccione una opción: ")
-        
-        if opcion == "1":
-            user_management.registrarse()
-        elif opcion == "2":
-            usuario = user_management.iniciar_sesion()
-            if usuario:
-                mensaje_bienvenida = "¡Bienvenido, {}!".format(usuario["correo"])
-                print(mensaje_bienvenida)
-                menu_tutorias(usuario)
-        elif opcion == "3":
-            print("¡Hasta luego!")
-            break
-        else:
-            print("Opción inválida. Por favor, inténtelo de nuevo.")
+    elif opcion == "3":
+        print("¡Hasta luego!")
+    else:
+        print("Opción inválida. Por favor, inténtelo de nuevo.")
 
-def menu_tutorias(usuario):
-    while True:
-        print("\nMenú de Tutorías:")
-        print("1. Ver clases disponibles para tutorías")
-        print("2. Ver tutorías asignadas")
-        print("3. Dejar un comentario")
-        print("4. Cerrar sesión")
-        
-        opcion = input("Seleccione una opción: ")
-        
-        if opcion == "1":
-            tutoring_management.ver_clases_disponibles()
-        elif opcion == "2":
-            tutoring_management.ver_tutorias_asignadas(usuario)
-        elif opcion == "3":
-            tutoring_management.dejar_comentario(usuario)
-        elif opcion == "4":
-            print("Cerrando sesión...")
-            break
-        else:
-            print("Opción inválida. Por favor, inténtelo de nuevo.")
-
-if __name__ == "__main__":
-    main()
